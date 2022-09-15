14f2d80c876b73755bc1b302a634a118f9288002
adicwu <youzaiwu@qq.com>
2022-09-15 16:45:36 +0800

    chore: build优化

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { BASE_REQUEST_URL } from "./src/common/request/static";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve("./src"),
