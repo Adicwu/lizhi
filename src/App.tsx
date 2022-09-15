98b241755cdd555dd56fd86b23786fcf539c14c6
adicwu <youzaiwu@qq.com>
2022-09-15 16:12:16 +0800

    feat: 自适应窗口高度

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { CSSProperties, useMemo, useRef } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.less";
 import Login from "./views/Login/Index";
+import { useWindowSize } from "./hooks/useWindowSize";
 
 function App() {
+  const [windowSize] = useWindowSize();
+  const appStyle = useMemo<CSSProperties>(
+    () => ({
+      height: `${windowSize.height}px`,
+    }),
+    [windowSize]
+  );
   return (
-    <div className="app">
+    <div className="app" style={appStyle}>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
